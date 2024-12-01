190e70bb07360c2b001f6fb390fc7ead7b40fe5b
Anton Haba <lykoiiiko@gmail.com>
2024-12-01 15:33:28 +0300

    complite 9/10

--- sw1/script.js
+++ sw1/script.js
@@ -97,11 +97,98 @@ console.log(randomElement(arr5));
 console.log('---------------Задание 6----------------');
 //6. Шифр Цезаря: Напишите функцию, которая шифрует латинскую строку с помощью шифра Цезаря с заданным сдвигом. Например: "table" со сдвигом 2 будет "vcdng". Каждая буква "table" имеет свой номер в таблице ASCI (116, 97, 98, 108, 101). Создайте вторую функцию, которая будет расшифровывать текст, если передать правильный ключ сдвига.
 
+function caesarCoder(str6, key) {
+	str6Coded = "";
+
+	for(let i = 0; i < str6.length; i++) {
+		let symbol = str6[i].charCodeAt(),
+			symbolCoded = symbol + key,
+			symbolCodedLetter = String.fromCharCode(symbolCoded);
+
+		str6Coded += symbolCodedLetter;
+	}
+	
+	return str6Coded;
+}
+
+let str6 = "table";
+console.log(caesarCoder(str6, 2)); //шифруем строку
+
+function caesarDecoder(str6Coded, key) {
+	let str6Decoded = "";
+
+	for(let i = 0; i < str6Coded.length; i++) {
+		let symbol = str6Coded[i].charCodeAt(),
+			symbolCoded = symbol - key,
+			symbolCodedLetter = String.fromCharCode(symbolCoded);
+
+		str6Decoded += symbolCodedLetter;
+	}
+	
+	return str6Decoded;
+}
+
+console.log(caesarDecoder(str6Coded, 2)); //расшифровываем код
+
 console.log('---------------Задание 7----------------');
 //7. Напишите программу, которая генерирует случайным образом новый пароль, состоящий из 8 чисел (параметром можно задавать длину) и возвращает результат. По желанию, доработайте функцию: сделайте генератор паролей из латинских символов, целых чисел и специальных символов: _-,.&*^$#@+=!; минимум один большой символ, одна цифра, один спец. символ.
 
+function getPass(length) {
+	let pass = '';
+
+	for(let i = 0; i < length; i++) {
+		let num = Math.round(Math.random() * 9);
+		pass += num;
+	}
+
+	return pass;
+}
+
+console.log(getPass(8));
+
 console.log('---------------Задание 8----------------');
 //8. Создайте функцию, которая создает произвольный HTML-элемент в <body> в виде круга/овала/квадрата/прямоугольника. Функция должна принимать параметры: width, height, radius, color; создавать запись "<div style="width: 200px; height: 100px, border-radius: 5px; background-color: red;"></div>". Данное значение добавляйте через document.write. Подумайте о том, как сделать случайную генерацию фигур по вызову функции без параметров.
+
+function printElement(width, height, radius, color) {
+	if (!width && !height && !radius && !color) { //генерация рандомной фигуры
+		let width = Math.round(Math.random() * (300 - 100) + 100),
+			height = Math.round(Math.random() * (300 - 100) + 100),
+			radius = Math.round(Math.random() * 50),
+			colorIndex = Math.round(Math.random() * 4),
+			colorArr = ['blue', 'red', 'green', 'black', 'yellow'],
+			color = colorArr[colorIndex];
+		console.log(colorIndex, color);
+		document.write(`<div style="width: ${width}px; height: ${height}px; border-radius: ${radius}%; background-color: ${color};"></div>`);
+	} else {
+		document.write(`<div style="width: ${width}; height: ${height}; border-radius: ${radius}; background-color: ${color};"></div>`);
+	}
+}
+
+printElement(); //выводим рандомную фигуру
+
+let width = '200px',
+	height = '200px',
+	radius = '50%',
+	color = 'black';
+printElement(width, height, radius, color); //круг
+
+width = '400px',
+height = '200px',
+radius = '50%',
+color = 'red';
+printElement(width, height, radius, color); //овал
+
+width = '200px',
+height = '200px',
+radius = '0%',
+color = 'blue';
+printElement(width, height, radius, color); //квадрат
+
+width = '300px',
+height = '200px',
+radius = '0%',
+color = 'green';
+printElement(width, height, radius, color); //прямоугольник
 
 console.log('---------------Задание 9----------------');
 //9. [18+] Создайте функцию-цензор, которая проверяет текст на наличие нецензурных слов и заменяет их на "#". 
@@ -114,5 +201,85 @@ console.log('---------------Задание 9----------------');
 
 //Предусмотрите возможность расширения проверяемых слов в тексте.
 
+function censorFeature(str9) {
+	let censorWords = new RegExp('нахрен|черт|бляха', 'gmi'); //здесь можно добавить нецензурные слова
+	let str9Censored = str9.replace(censorWords, "####");
+
+	return str9Censored;
+}
+
+let str9 = 'В сером городе, где каждый день напоминал предыдущий, одиночество казалось нестерпимым. Люди сливались в одну серую массу, а Яна, сжав кулаки, прокладывала путь сквозь толпу. "Какая же нахрен жизнь!" – думала она, чувствуя, как ветер шутит с ее волосами. Бросив взглянуть на небо, она заметила, что даже облака выглядят опустошенно. "Почему у всех так легко? Почему я, черт побери, должна каждый раз зажмуриваться от этой скуки?" Она присела на старую скамейку в парке и, посмотрев на прохожих, вдруг почувствовала, как на душе легло тяжелое бремя. "Да нахрен с ним, может, стоит просто сбежать?" – прошептала она, уже представляя, как дерзко покидает этот серый мир. В её сердце разгорелось пламя, и, поднимаясь, она с готовностью шагнула навстречу жизни. Ей не нужны были никакие фальшивые улыбки, лишь свобода и возможность быть собой. Бляха, она заслуживала этого! И, стиснув зубы, уверенно направилась в неизвестность, где ждала её настоящая сказка.';
+
+console.log(censorFeature(str9));
+
+
 console.log('---------------Задание 10----------------');
 //10. Создайте функцию-конструктор или отдельный простой объект "Тамагочи" с минимальным набором функций: включение, выключение, вывод общей информации о состоянии (имя, дата рождения, здоровье по 5-бальной шкале, сытость по 5 шкале, сон в часах от 0 до 8), покормить, спать. При включении "друга" запрашивается имя, автоматически добавляется дата рождения, и все показатели состояния устанавливаются в максимальные значения. Каждую минуту (или 1 час, как вы захотите), показатель сытости уменьшается на одну единицу. Каждый раз, когда сытость доходит до 1 и меньше, то уменьшается здоровье на 1 единицу. Если "друг" не спал больше 6 часов, то каждый последующий час здоровье уменьшается на 1 единицу. Чтобы восстановить здоровье, нужно быть сытым и хорошо спать: при каждом вызове метода "покормить" или "спать" соотв. параметры увеличиваются на 1 единицу. Каждый раз, когда параметры сон и сытость приходят к значению 5, то здоровье увеличивается на 1 единицу, но не может превышать максимального значения. По вашему желанию, функции тамагочи могут быть улучшены или добавлены новые. Проявите фантазию!
+
+function Tamagotchi() {
+    this.name = '',
+	this.birthDate = '',
+    this.health = 5,
+    this.hunger = 5,
+    this.sleep = 8,
+    this.isOn = false,
+
+    this.turnOn = function() {
+        this.isOn = true;
+        this.name = prompt("Введите имя вашего Тамагочи:");
+        this.birthDate = new Date().toLocaleString();
+        this.health = 5;
+        this.hunger = 5;
+        this.sleep = 8;
+        setInterval(() => this.decreaseHunger(), 60000); // decrease hunger every minute
+    },
+
+    this.turnOff = function() {
+        this.isOn = false;
+        clearInterval();
+    },
+
+    this.decreaseHunger = function() {
+        if (this.hunger > 1) {
+            this.hunger--;
+        } else {
+            this.hunger = 0;
+            if (this.health > 1) {
+                this.health--;
+            }
+        }
+    },
+
+    this.feed = function() {
+        if (this.hunger < 5) {
+            this.hunger++;
+        }
+        if (this.sleep < 5) {
+            this.sleep++;
+        }
+    },
+
+    this.sleep = function() {
+        if (this.sleep < 8) {
+            this.sleep++;
+        }
+        if (this.hunger < 5) {
+            this.hunger++;
+        }
+    },
+
+    this.showInfo = function() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Birth Date: ${this.birthDate}`);
+        console.log(`Health: ${this.health}/5`);
+        console.log(`Hunger: ${this.hunger}/5`);
+        console.log(`Sleep: ${this.sleep}`);
+    }
+};
+
+let tamagotchi = new Tamagotchi();
+
+tamagotchi.turnOn();
+// Tamagotchi.feed();
+// Tamagotchi.sleep();
+tamagotchi.showInfo();
