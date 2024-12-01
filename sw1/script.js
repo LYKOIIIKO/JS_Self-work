b03f4571ad33ba80cce581f6c0585d564ef0e641
Anton Haba <lykoiiiko@gmail.com>
2024-12-01 12:25:59 +0300

    complite 5/10

--- sw1/script.js
+++ sw1/script.js
@@ -3,17 +3,96 @@
 console.log('---------------Задание 1----------------');
 //1. Напишите функцию, которая добавляет в существующий массив (можно пустой) города Беларуси. Если в качестве параметра ничего не передается в функцию, то при каждом ее вызове, новый город запрашивается через prompt, иначе новые города можно добавить одним параметром - текстом городов через запятую. Выведите получившийся массив в алфавитном порядке.
 
+function getCity(city) {
+	if (!city) {
+		city = prompt('Введите название города...');
+		arrCity.push(city);
+	} else {
+		arrCity = city.split(',');
+	}
+}
+
+let arrCity = [];
+// getCity('Минск,Брест,Гродно,Витебск,Могилев,Гомель');
+// getCity();
+// getCity();
+console.log(arrCity.sort());
+
 console.log('---------------Задание 2----------------');
 //2. Создайте двумерный массив [['Иван', 'Катя', 'Ольга', 'Максим'], ['Минск', 'Брест', 'Гродно', 'Могилев']]. Используя вложенные циклы вывести в консоль строки вида: "Иван из Минск", "Катя из Брест", "Ольга из Гродно", "Максим из Могилев". Создайте массив с расстояниями до городов от Минска (нулевой км). Выведите информацию о расстоянии между Иваном и Максимом. Предусмотрите возможность запрашивать расстояние между другими людьми.
+
+// let arr2 = [['Иван', 'Катя', 'Ольга', 'Максим'], ['Минск', 'Брест', 'Гродно', 'Могилев']];
+
+// for(let i = 0; arr2[0][i] != undefined || arr2[1][i] != undefined; i++) {
+// 	console.log(`${arr2[0][i]} из ${arr2[1][i]}`);
+// }
+
+// let distanceArr = [0, 351, 276, 197];
+
+// let distanceBetweenStr = prompt('Вы можете узнать расстояние между людьми! Введите два имени через запятую (прим.:Иван,Максим).'),
+// 	distanceBetweenArr = distanceBetweenStr.split(','),
+// 	distanceBetweenName1 = arr2[0].indexOf(distanceBetweenArr[0]),
+// 	distanceBetweenName2 = arr2[0].indexOf(distanceBetweenArr[1]),
+// 	distanceBetweenPeople = distanceArr[distanceBetweenName1] + distanceArr[distanceBetweenName2];
+
+// console.log(`Расстояние между ${arr2[0][distanceBetweenName1]} и ${arr2[0][distanceBetweenName2]} равняется ${distanceBetweenPeople}км (если ехать через Минск)`);
 
 console.log('---------------Задание 3----------------');
 //3. Напишите функцию, которая принимает две даты и возвращает количество дней между ними. В зависимости от значения, выведите: "дата уже близко" (< 3 дней), "еще есть время" (от 3 до 7), "далековато еще" (> 7 дней).
 
+function daysBetweenDates(date1, date2) {
+	let dateStart = new Date(date1),
+		dateEnd = new Date(date2),
+		dateBetweenMS = dateStart - dateEnd, //разница в миллисекундах
+		dateBetweenDays = dateBetweenMS / (1000 * 3600 * 24),//разница в днях
+		dateBetweenDaysReturn = Math.abs(Math.round(dateBetweenDays));
+
+	if(dateBetweenDaysReturn < 3) {
+		return console.log(`Разница между датами ${date1} и ${date2} составляет ${dateBetweenDaysReturn} дня(ей). Дата уже близко.`);
+	} else if(dateBetweenDaysReturn >= 3 && dateBetweenDaysReturn < 7){
+		return console.log(`Разница между датами ${date1} и ${date2} составляет ${dateBetweenDaysReturn} дня(ей). Еще есть время.`);
+	} else if(dateBetweenDaysReturn >= 7){
+		return console.log(`Разница между датами ${date1} и ${date2} составляет ${dateBetweenDaysReturn} дня(ей). Далековато еще.`);
+	}
+	
+}
+
+daysBetweenDates('2024.12.01', '2024.11.29');
+
 console.log('---------------Задание 4----------------');
 //4. Напишите функцию celsiusToFahrenheit(celsius), которая конвертирует температуру из градусов Цельсия в градусы Фаренгейта. В зависимости от температуры, выведите предупреждение: "сегодня прохладнее, чем обычно" (от 5 до 10), "одевайтесь теплее" (от 0 до 5), "сегодня очень холодно" (меньше 0 и до -5), "оставайтесь дома" (< -5).
 
+function celsiusToFahrenheit(celsius) {
+	let fahrenheit = (9 / 5) * celsius + 32;
+
+	if(celsius > 5 && celsius <= 10) {
+		return console.log(`Температура за бортом ${celsius}\u2103 / ${fahrenheit}\u2109. Сегодня прохладнее, чем обычно.`);
+	} else if (celsius > 0 && celsius <= 5) {
+		return console.log(`Температура за бортом ${celsius}\u2103 / ${fahrenheit}\u2109. Одевайтесь теплее.`);
+	} else if (celsius > -5 && celsius <= 0) {
+		return console.log(`Температура за бортом ${celsius}\u2103 / ${fahrenheit}\u2109. Сегодня очень холодно.`);
+	} else if (celsius <= -5) {
+		return console.log(`Температура за бортом ${celsius}\u2103 / ${fahrenheit}\u2109. Оставайтесь дома.`);
+	}
+	
+}
+celsiusToFahrenheit(10);
+celsiusToFahrenheit(4);
+celsiusToFahrenheit(0);
+celsiusToFahrenheit(-6);
+
 console.log('---------------Задание 5----------------');
 //5. Функция выбора случайного элемента из массива: Создайте функцию randomElement(arr), которая возвращает случайный элемент из переданного массива.
+
+function randomElement(arr5) {
+	let arrRandom = Math.floor(Math.random() * arr5.length); 
+	
+	return arr5[arrRandom];
+}
+
+let arr5 = ['Петя', 15, "Абракадабра", 1998, "Полтергейст", "Назад в будущее", "Хроники Риддика", "Вася Пупкин", 22333, ["вложенный массив",2,3,4]];
+
+console.log(randomElement(arr5));
 
 console.log('---------------Задание 6----------------');
 //6. Шифр Цезаря: Напишите функцию, которая шифрует латинскую строку с помощью шифра Цезаря с заданным сдвигом. Например: "table" со сдвигом 2 будет "vcdng". Каждая буква "table" имеет свой номер в таблице ASCI (116, 97, 98, 108, 101). Создайте вторую функцию, которая будет расшифровывать текст, если передать правильный ключ сдвига.
